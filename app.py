7ba0bb6c47d28b034506a461fd7255a7bb273aa3
Kevin Pratama <kvpratama@gmail.com>
2023-12-01 09:58:06 +0900

    summarize single web page

--- app.py
+++ app.py
@@ -1,15 +1,8 @@
-import os
-from flask import Flask, render_template, request, flash, redirect, session, send_from_directory
-# from datetime import datetime
+from flask import Flask, render_template, request
 from flask_session import Session
-# from werkzeug.security import check_password_hash, generate_password_hash
-# from helpers import apology, login_required, admin_required
 from project import find_news, scraping, inference, is_paragraph
-import json
 
 app = Flask(__name__)
-# UPLOAD_FOLDER = 'uploads'
-# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
@@ -62,6 +55,40 @@ def find():
 
             # print("\n", "#" * 50)
     return render_template('articles.html', articles=articles, topic=topic)
+
+
+@app.route('/summarize')
+def summarize():
+
+    return render_template('summarize.html')
+
+
+@app.route('/summary', methods=["POST"])
+def summary():
+    url = request.form.get("url")
+
+    article = {'url': url}
+    payload, summary = "", ""
+    paragraph_counter, max_n_paragraph, sum_every_n = 0, 9, 5
+    for i, text in enumerate(scraping(url)):
+        if is_paragraph(text):
+            paragraph_counter += 1
+            # print(i, paragraph_counter)
+            payload += text
+            if i % sum_every_n == sum_every_n-1:
+                # print(payload)
+                query = {"inputs": payload}
+                summary += inference(query)[0]['summary_text'] + "\n"
+                payload = ""
+            if paragraph_counter == max_n_paragraph:
+                break
+
+    if payload:
+        query = {"inputs": payload}
+        summary += inference(query)[0]['summary_text']
+    article['summary'] = summary
+
+    return render_template('summary.html', article=article)
 
 
 if __name__ == '__main__':
