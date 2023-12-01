2a4429621bb72031a83b2e7af7f152c4554bc2f0
Kevin Pratama <kvpratama@gmail.com>
2023-12-01 10:11:06 +0900

    test_project

--- test_project.py
+++ test_project.py
@@ -1,6 +1,19 @@
 from project import *
 
 
+def test_inference():
+    payload = {"inputs": "This is a test."}
+    assert inference(payload)
+
+
+def test_scraping():
+    assert scraping("https://www.google.com")
+
+
+def test_find_news():
+    assert find_news("covid")
+
+
 def test_is_paragraph():
     assert is_paragraph(
         "This is a paragraph with more than 15 words. This is a paragraph with more than 15 words.")
